df6abfb94f907c89ca2e0c478e02a1755e20cc80
Kaiju <domenicjohn.aligaen@students.isatu.edu.ph>
2025-03-19 10:20:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-function limitWords(event) {
-    const input = event.target.value;
-    const words = input.trim().split(/\s+/); 
-    if (words.length > 20) {
-      
-      event.target.value = words.slice(0, 20).join(' ');
-    }
-  }
 
-  function preventAdditionalTyping(event) {
-    const input = event.target.value;
-    const words = input.trim().split(/\s+/);
-    if (words.length >= 20 && event.key !== "Backspace" && event.key !== "Delete") {
-      event.preventDefault(); 
-    }
+function limitWords(event) {
+  const maxWords = 100; 
+  const words = event.target.value.split(/\s+/);
+  
+  if (words.length > maxWords) {
+
+      event.target.value = words.slice(0, maxWords).join(" ");
   }
+}
+
+function preventAdditionalTyping(event) {
+  const maxWords = 100;
+  const words = event.target.value.split(/\s+/);
+  
+  if (words.length > maxWords) {
+      event.preventDefault(); 
+  }
+}
+
+// Get Text from the textarea
+
+const txt1 = document.getElementById('input');
+const btn1 = document.getElementById('button');
+const out1 = document.getElementById('output');
+
+function fun1() {
+    out1.innerHTML = txt1.value;
+}
+
+btn1.addEventListener('click', fun1);
